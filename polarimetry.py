9177836c3c79223a0a48d586660de2400ec3e3e8
Prem Vijay V <35184933+premvijay@users.noreply.github.com>
2020-02-29 22:55:27 +0530

    works for set 1 but doesn't for set 3 due to parameter choice in star finding
    Correction factor and error needs to be implimented

--- polarimetry.py
+++ polarimetry.py
@@ -14,46 +14,52 @@ from photutils import DAOStarFinder, aperture_photometry, CircularAperture
 from astropy.stats import mad_std
 #import aplpy
 
-fits_file = r"C:\Users\premv\Documents\acad\gradschool\term-3\Astrotech-I\Experiments\Exp-1\Data\Polarized\Set1_0.FIT"
 
-#gc = aplpy.FITSFigure(fits_file)
-
-#gc.show_grayscale()
-
-hdul = fits.open(fits_file)
-image = np.float64(hdul[0].data)
-x_pixels = image.shape[0] * u.pix
-fwhm = 16.
-
-image -= np.median(image)
-bkg_sigma = mad_std(image)
-daofind = DAOStarFinder(fwhm=fwhm, threshold=10.*bkg_sigma)  
-sources = daofind(image)  
-for col in sources.colnames:  
-    sources[col].info.format = '%.8g'  # for consistent table output
-print(sources)  
-
-positions = np.transpose((sources['xcentroid'],sources['ycentroid']))
-
-apertures = CircularAperture(positions,r=fwhm)
+fits_dir = "C:\\Users\\premv\\Documents\\acad\\gradschool\\term-3\\Astrotech-I\\Experiments\\Exp-1\\Data\\"
+fits_subdir = "Polarized\\"
 
 
-
-plt.imshow(image)
-apertures.plot()
-
-photo_apertures = aperture_photometry(image, apertures)
-
-print(photo_apertures)
+setno = 1
+R = []
 
 
-
-
-
-N0 = np.max(photo_apertures['aperture_sum'][photo_apertures['xcenter']<x_pixels/2])
-N1 = np.max(photo_apertures['aperture_sum'][photo_apertures['xcenter']>x_pixels/2])
-
-R = (N0-N1)/(N0+N1)
+for alpha in list(range(0,900,900//4)):
+#alpha = 0
+    
+    fits_filename = "Set{0}_{1}.FIT".format(setno, alpha)
+    fits_filepath = fits_dir + fits_subdir + fits_filename
+    
+    #gc = aplpy.FITSFigure(fits_file)
+    
+    #gc.show_grayscale()
+    
+    hdul = fits.open(fits_filepath)
+    image = np.float64(hdul[0].data)
+    x_pixels = image.shape[0] * u.pix
+    fwhm = 16.
+    
+    image -= np.median(image)
+    bkg_sigma = mad_std(image)
+    daofind = DAOStarFinder(fwhm=fwhm, threshold=10.*bkg_sigma)  
+    sources = daofind(image)  
+    for col in sources.colnames:  
+        sources[col].info.format = '%.8g'  # for consistent table output
+    print(sources)  
+    
+    positions = np.transpose((sources['xcentroid'],sources['ycentroid']))
+    
+    apertures = CircularAperture(positions,r=fwhm)
+    
+    plt.imshow(image)
+    apertures.plot()
+    
+    photo_apertures = aperture_photometry(image, apertures)
+    print(photo_apertures)
+    
+    N0 = np.max(photo_apertures['aperture_sum'][photo_apertures['xcenter']<x_pixels/2])
+    N1 = np.max(photo_apertures['aperture_sum'][photo_apertures['xcenter']>x_pixels/2])
+    
+    R.append((N0-N1)/(N0+N1))
 
 
 
